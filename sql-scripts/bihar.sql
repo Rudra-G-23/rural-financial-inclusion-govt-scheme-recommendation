a3af494f98a6400ad762dd7d75e43b6db4736b36
Rudra Prasad Bhuyan <rudraprasadbhuyan999@gmail.com>
2026-01-17 21:31:38 +0530

    Bihar subset and `hh_unique_id` created and merge successfully

---
+++ sql-scripts/bihar.sql
@@ -0,0 +1,7 @@
+-- select one section 
+SELECT * FROM analytics.level_09_section_9_10_11
+WHERE "State" = '10'  AND "FSU_Serial_No" = 27000;
+
+-- find how many rows 
+SELECT count(*) FROM analytics.level_05_sec_5_6
+WHERE "State" = '10'; 
