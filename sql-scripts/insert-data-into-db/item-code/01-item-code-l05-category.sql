5dcf9c278ecb5ea71c1f3df9928ccc924177192b
Rudra Prasad Bhuyan <rudraprasadbhuyan999@gmail.com>
2026-02-02 21:24:20 +0530

    Item dict to DF

---
+++ sql-scripts/insert-data-into-db/item-code/01-item-code-l05-category.sql
@@ -0,0 +1,21 @@
+CREATE TABLE item_category_map (
+    item_code INTEGER PRIMARY KEY,
+    category TEXT
+);
+
+INSERT INTO item_category_map VALUES
+(129,'cereals'),
+(139,'cereal substitute'),
+(159,'pulses & products'),
+(179,'salt & sugar'),
+(169,'milk & milk products'),
+(219,'vegetables'),
+(239,'fruits (fresh)'),
+(249,'fruits (dry)'),
+(199,'egg, fish & meat'),
+(189,'edible oil'),
+(269,'spices'),
+(279,'beverages'),
+(289,'served processed food'),
+(299,'packaged processed food');
+
